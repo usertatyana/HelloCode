f65e8708959c4febe35f8cbf7d30d008529e860e
Tatyana Kapina <deniscapin@yandex.ru>
2022-08-04 23:41:18 +0300

    random

--- Examples002_HelloUser/Program.cs
+++ Examples002_HelloUser/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введите ваше имя ");
+﻿Console.Write("Введите свое имя ");
 string username = Console.ReadLine();
-Console.Write("Привет, ");
+Console.Write("Привет, привет ");
 Console.Write(username);
