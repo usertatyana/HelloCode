dfd40428786c496f7bab25da9a24e9df97586849
Tatyana Kapina <deniscapin@yandex.ru>
2022-08-11 13:33:27 +0300

    Example 010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 25, 35, 14, 25, 68, 7, 68};
+
+int n = array.Length;
+int find = 68;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
